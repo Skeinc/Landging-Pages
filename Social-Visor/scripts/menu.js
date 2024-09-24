8b7c6ddec1edc0b2c9308efdc1361f81f871b69a
Anufriev Dmitry <skezydeveloper@gmail.com>
2024-09-24 23:58:04 +0300

    done hero section

---
+++ Social-Visor/scripts/menu.js
@@ -0,0 +1,7 @@
+// Переключатель меню
+const menuToggler = document.querySelector('#menu');
+
+// Обработка нажатия на переключатель меню
+function menuTogglerHandler() {
+    menuToggler.classList.toggle('menu-close');
+}
